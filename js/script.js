0f2e450214b019e8b0073d7d155ab66da14ad7c9
Micaela <94004537+micaela24c@users.noreply.github.com>
2022-02-24 11:33:06 -0500

    nuevo form

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDsHxQbdA3frafHvG5O6LEfBS2vgGjl2y0",
-    authDomain: "inaqua2-9e4fd.firebaseapp.com",
-    projectId: "inaqua2-9e4fd",
-    storageBucket: "inaqua2-9e4fd.appspot.com",
-    messagingSenderId: "635208619390",
-    appId: "1:635208619390:web:3505fbe0aec585da0f8f2f",
-    measurementId: "G-3XLN587HXP"
-  };
+  apiKey: "AIzaSyDsHxQbdA3frafHvG5O6LEfBS2vgGjl2y0",
+  authDomain: "inaqua2-9e4fd.firebaseapp.com",
+  projectId: "inaqua2-9e4fd",
+  storageBucket: "inaqua2-9e4fd.appspot.com",
+  messagingSenderId: "635208619390",
+  appId: "1:635208619390:web:3505fbe0aec585da0f8f2f",
+  measurementId: "G-3XLN587HXP"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // Reference messages collection
-var messagesRef = firebase.database().ref('messages');
+//var messagesRef = firebase.database().ref('messages');
 
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 
 // Submit form
-function submitForm(e){
+function submitForm(e) {
   e.preventDefault();
 
   // Get values
@@ -33,26 +33,38 @@ function submitForm(e){
   document.querySelector('.alert').style.display = 'block';
 
   // Hide alert after 3 seconds
-  setTimeout(function(){
+  setTimeout(function () {
     document.querySelector('.alert').style.display = 'none';
-  },3000);
+  }, 3000);
 
   // Clear form
   document.getElementById('contactForm').reset();
 }
 
 // Function to get get form values
-function getInputVal(id){
+function getInputVal(id) {
   return document.getElementById(id).value;
 }
 
 // Save message to firebase
-function saveMessage(name, email, phone, message){
-  var newMessageRef = messagesRef.push();
-  newMessageRef.set({
+function saveMessage(name, email, phone, message) {
+  /* var newMessageRef = messagesRef.push();
+   newMessageRef.set({
+     name: name,
+     email:email,
+     phone:phone,
+     message:message
+   }); */
+  firebase.db.collection("messages").add({
     name: name,
-    email:email,
-    phone:phone,
-    message:message
-  });
+    email: email,
+    phone: phone,
+    message: message
+  })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
 }
